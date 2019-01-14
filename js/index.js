c64cea3483a898cd956b2ccbb88807d123c19149
c-zouzhongxing1 <c-zouzhongxing1@SZ_ZOUZHONGXING>
2019-01-14 21:09:39 +0800

    人员名单

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-//抽奖人员名单
-var allPerson = "蔡国瑜;曹正一;曾隆海;曾耀衡;陈观斌;陈军;陈军俊;陈倩雯;陈毓新;崔惠海;戴锦霞;段斌;樊清华;方林;符运琼;付坤;付文静;付园园;龚小龙;古冬苗;古举标;官鑫;何若兵;洪俊凯;侯斌;侯莉;胡军;胡伟澎;胡晓;黄欢茂;黄玲;黄星心;黄泽辉;黄志博;蒋明;金矿;赖礼通;赖婷婷;兰方权;李成国;李国庆;李吉庆;李兰兰;李良;李鹏程;李胜康;李涛;李未波;李咸良;李小红;李鑫;李焱;梁家寶;林珊珊;林伟俊;刘婧娟;刘玫;刘权;刘小燕;刘新星;刘曜玮;刘奕;卢丽花;罗宇峰;吕春杰;苗继业;莫忧;南良改;潘志健;彭海锋;彭科达;彭钟涛;乔新;石浩;宋超;宋浩;苏鸿;谭秀梅;田冰;田晶;田力玮;汪鑫;王刚正;王晶;王立伟;王石林;王政阳;韦振勇;吴海荣;吴家胜;吴俊;吴晏琳;向真明;徐良;许海芬;闫海燕;杨广霞;杨力平;杨涛;杨志明;叶辰;叶华浩;殷雪;张丹丹;张华宁;张辉武;张娟;张敏;张清云;张勇;张志强;郑大鹏;周萍;朱然威;李铎;崔丽洁;吴耀红;温先木;李奕邦;郭学端;李伟;刘玉灼;周成威;盛子凡;刘佳;刘宇航;曾冬资;王巍;张晶;邱东;陈龙;郑威;刘伟雄;叶丽娟;谭剑颖;丁鹏;李江洲;姜萌萌;彭华婴;李德林;黎宇;叶强;师新会;冯绍文;林海强;潘冰冰;彭见峡;卢军良;江山;王义;甘伊璇;刘倩;龚京栋;闵冬;张宜羡;徐龙瑞;邹中兴;廖华衡;胡斌;周孝雄;时攀;苏路凯;李丽冲;彭启;曹安琥;廖宜源;黄荣发;严小锋;郭春艳";
+﻿//抽奖人员名单
+var allPerson = "张三;李四;陈五;王六;蔡七;赖八";
 //领导人员名单
-var leaderArr = ["方林", "宋浩", "叶辰", "田力玮", "樊清华", "张勇", "蒋明", "官鑫", "李国庆", "赖礼通", "石浩", "杨广霞"];
+var leaderArr = ["张三"];
 //未中奖人员名单
 var remainPerson = allPerson.toString().split(";");
 //中奖人员名单
@@ -78,11 +78,15 @@ function startLuckDraw() {
     var randomPerson = getRandomArrayElements(remainPerson, luckyDrawNum);
     var tempHtml = "";
     $.each(randomPerson, function (i, person) {
+        var sizeStyle = "";
+        if (person.length > 3) {
+            sizeStyle = " style=font-size:" + 3 / person.length + "em";
+        }
         if (leaderArr.indexOf(person) > -1 && times == 1) {
-            tempHtml += "<span><b>" + person + "</b></span>";
+            tempHtml += "<span><span " + sizeStyle + "><b>" + person + "</b></span></span>";
         }
         else {
-            tempHtml += "<span>" + person + "</span>";
+            tempHtml += "<span><span " + sizeStyle + ">" + person + "</span></span>";
         }
     });
     $("#result>div").html(tempHtml);
